0d76209e2d41586d7df85d905e95391961ae0a55
Le-Tien-Dung <letiendung911@gmail.com>
2025-04-02 22:33:17 +0700

    Add files via upload

---
+++ Untitled4.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main() {
+	float pi=3.14, R;
+	printf("nhap ban kinh:");
+	scanf("%f", &R);
+	float S,V;
+	S= 4*pi*R*R;
+	V= 4/3*pi*R*R*R;
+	printf("dien  tich hcau la:%f, the tich hinh cau la:%f", S, V);
+}
